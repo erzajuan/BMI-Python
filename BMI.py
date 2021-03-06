dfcb8b2fc3737c4c7aa491204a6e3be5e400b5f3
erzajuan <direkal2@gmail.com>
2021-03-06 19:59:23 +0700

    Create BMI.py

---
+++ BMI.py
@@ -0,0 +1,20 @@
+def main():
+    beratBadan = int(input("Berat Badan (kg)    : "))
+    tinggiBadan = int(input("Tinggi Badan (cm)  : "))
+    print("Dengan Berat Badan {}kg dan Tinggi Badan {}cm".format(
+        beratBadan, tinggiBadan))
+    BMI = float(beratBadan*10000/(tinggiBadan*tinggiBadan))
+    print("BMI anda adalah  : {:.2f}".format(BMI))
+
+    if BMI < 18.5:
+        print("Anda Termasuk Kekurangan Berat badan")
+    elif 25 > BMI >= 18.5:
+        print("Anda Termasuk Normal")
+    elif 30 > BMI >= 25:
+        print("Anda Termasuk Kelebihan Berat Badan")
+    else:
+        print("Anda Termasuk Obesistas")
+
+
+if __name__ == '__main__':
+    main()
