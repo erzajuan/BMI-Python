b60b2f14520bcac99d80e917372f877b30c408a6
erzajuan <49344840+erzajuan@users.noreply.github.com>
2021-03-06 20:00:51 +0700

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -4,7 +4,6 @@ def main():
     print("Dengan Berat Badan {}kg dan Tinggi Badan {}cm".format(beratBadan, tinggiBadan))
     BMI = float(beratBadan*10000/(tinggiBadan*tinggiBadan))
     print("BMI anda adalah  : {:.2f}".format(BMI))
-
     if BMI < 18.5:
         print("Anda Termasuk Kekurangan Berat badan")
     elif 25 > BMI >= 18.5:
